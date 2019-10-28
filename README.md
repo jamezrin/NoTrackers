4bdc62d8eaf36c72c9e2782fecbbbc7a11a3094e
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-10-28 21:03:39 +0100

    Add tests for query string parser

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Chrome extension to skip tracking services that redirect you to the target url.
 
 This is specially useful if you use a DNS AdBlocker which has these services blocked.
 In cases like this, if you didn't use this extension, you'd end up unable to navigate to the desired site.
+
+### Executing tests
+
+To run tests execute `mocha` in the `test` directory
---
+++ test/main.js
@@ -0,0 +1,59 @@
+var extension = require('../main');
+var assert = require('assert');
+
+describe('Query String Parsing', function () {
+    it('should parse url with no parameters correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some"),
+            {}
+        );
+    });
+    it('should parse url with no parameters correctly, with interrogation mark', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?"),
+            {}
+        );
+    });
+    it('should parse url with one parameter without value correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?foo"),
+            { foo: "" }
+        );
+    });
+    it('should parse url with one parameter without value correctly, with equals sign', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?foo="),
+            { foo: "" }
+        );
+    });
+    it('should parse url with one parameter without key or value correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?="),
+            { "": "" }
+        );
+    });
+    it('should parse url with one valid parameter correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?foo=bar"),
+            { foo: "bar" }
+        );
+    });
+    it('should parse url with multiple parameters correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?foo=bar&azz=zza"),
+            { foo: "bar", azz: "zza" }
+        );
+    });
+    it('should parse url with one array parameter correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?foo=bar&foo=baz"),
+            { foo: ["bar", "baz"] }
+        );
+    });
+    it('should parse url with mixed parameter types correctly', function () {
+        assert.deepEqual(
+            extension.parseQueryString("http://some.site/some?foo=bar&foo=baz&oof=duh"),
+            { foo: ["bar", "baz"], oof: "duh" }
+        );
+    });
+});
